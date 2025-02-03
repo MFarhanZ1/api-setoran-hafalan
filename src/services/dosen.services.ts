a58cfe50270d6060ac2ce6796b31d9702ce07c15
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-03 21:07:44 +0700

    🛠 fix: solve img build n container start error

--- src/services/dosen.services.ts
+++ src/services/dosen.services.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { DosenHelper } from "../helpers/dosen.helpers";
+import { DosenHelper } from "../helpers/dosen.helpers.js";
 const prisma = new PrismaClient();
 
 class DosenService {
