7a69c7d6c23fe24aa6d647fb8df0a2f2a2dea123
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-03 13:29:58 +0700

    🔥 feat: add delete services setoran surah logic

--- src/services/dosen.services.ts
+++ src/services/dosen.services.ts
@@ -3,20 +3,20 @@ import { DosenHelper } from "../helpers/dosen.helpers";
 const prisma = new PrismaClient();
 
 class DosenService {
-  public static async getInfoDosenByEmail (email: string) {
-    return await prisma.dosen.findFirst({
-      where: {
-        email: email,
-      },
-      select: {
-        nama: true,
-        nip: true,
-      },
-    });
-  };
+	public static async getInfoDosenByEmail(email: string) {
+		return await prisma.dosen.findFirst({
+			where: {
+				email: email,
+			},
+			select: {
+				nama: true,
+				nip: true,
+			},
+		});
+	}
 
-  public static async getInfoMahasiswaPAPerAngkatanByEmail (email: string) {
-    const result = await prisma.$queryRaw`
+	public static async getInfoMahasiswaPAPerAngkatanByEmail(email: string) {
+		const result = await prisma.$queryRaw`
       SELECT 
           CONCAT('20', substring(nim FROM 2 FOR 2)) AS tahun, 
           COUNT(substring(nim FROM 2 FOR 2)) AS total 
@@ -30,15 +30,15 @@ class DosenService {
           substring(nim FROM 2 FOR 2) DESC
       LIMIT 8;
     `;
-  
-    return (result as { tahun: string; total: string }[]).map((item) => ({
-      ...item,
-      total: Number(item.total), // or item.total.toString() if you prefer strings
-    }));
-  };
 
-  public static async getMahasiswaPAByEmail (email: string) {
-    return await prisma.$queryRaw`
+		return (result as { tahun: string; total: string }[]).map((item) => ({
+			...item,
+			total: Number(item.total), // or item.total.toString() if you prefer strings
+		}));
+	}
+
+	public static async getMahasiswaPAByEmail(email: string) {
+		return await prisma.$queryRaw`
         SELECT 
           nim, nama
         FROM 
@@ -47,25 +47,34 @@ class DosenService {
           nip = (SELECT nip FROM dosen WHERE email = ${email})
         ORDER BY nama;
       `;
-  }
+	}
 
-  public static async checkExistingSetoran(nim: string, email_dosen_pa: string, nomorSurahInt: number) {
-    const result = await prisma.$queryRaw`
+	public static async checkExistingSetoran(
+		nim: string,
+		email_dosen_pa: string,
+		nomorSurahInt: number
+	) {
+		const result = await prisma.$queryRaw`
       SELECT * FROM "setoran" 
       WHERE "nim" = ${nim} 
         AND "nip" = (SELECT "nip" FROM "dosen" WHERE "email" = ${email_dosen_pa}) 
         AND "nomor_surah" = ${nomorSurahInt} 
       LIMIT 1
     `;
-    return !((result as any).length == 0);
-  }
-  
-  public static async postSetoran(nim: string, email_dosen_pa: string, nomor_surah: number, tgl_setoran: string) {
-    const idSetoran = DosenHelper.getIDSetoran();
-    const tglSetoran = tgl_setoran ? new Date(tgl_setoran) : new Date();
-    const tglValidasi = new Date();
-    
-    return await prisma.$queryRaw`
+		return !((result as any).length == 0);
+	}
+
+	public static async postSetoran(
+		nim: string,
+		email_dosen_pa: string,
+		nomor_surah: number,
+		tgl_setoran: string
+	) {
+		const idSetoran = DosenHelper.getIDSetoran();
+		const tglSetoran = tgl_setoran ? new Date(tgl_setoran) : new Date();
+		const tglValidasi = new Date();
+
+		return await prisma.$queryRaw`
       INSERT INTO 
         "setoran" ("id", "tgl_setoran", "tgl_validasi", "nim", "nip", "nomor_surah") 
       VALUES (
@@ -76,7 +85,15 @@ class DosenService {
         (SELECT nip FROM dosen WHERE email=${email_dosen_pa}), 
         ${nomor_surah}
       );
-    `;    
-  };
+    `;
+	}
+
+	public static async deleteSetoranByID(id_setoran: string) {
+		return await prisma.setoran.delete({
+			where: {
+				id: id_setoran,
+			},
+		});
+	}
 }
 export { DosenService };
