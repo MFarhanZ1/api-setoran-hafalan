c45f62eedb38ecd15fb971da9891eb6b90df4a0e
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-03 12:00:33 +0700

    ⚙ refactor: convert to OOP based and optimized more logic

--- src/services/dosen.services.ts
+++ src/services/dosen.services.ts
@@ -1,108 +1,82 @@
 import { PrismaClient } from "@prisma/client";
+import { DosenHelper } from "../helpers/dosen.helpers";
 const prisma = new PrismaClient();
 
-// Generate id untuk setoran
-class IDGenerator {
-  public static async generateNewIdSetoran(): Promise<string> {
-    const lastId = await prisma.setoran.findFirst({
-      orderBy: {
-        id: "desc",
+class DosenService {
+  public static async getInfoDosenByEmail (email: string) {
+    return await prisma.dosen.findFirst({
+      where: {
+        email: email,
       },
       select: {
-        id: true,
+        nama: true,
+        nip: true,
       },
     });
+  };
 
-    const prefix = "SH";
-    const currentYear = new Date().getFullYear().toString().slice(-2); // Tahun 2 digit terakhir
-    let newNumber: number;
-
-    if (lastId) {
-      const lastYear = lastId.id.slice(2, 4);
-      const lastNumber = parseInt(lastId.id.slice(4), 10);
-
-      if (lastYear === currentYear) {
-        newNumber = lastNumber + 1;
-      } else {
-        newNumber = 1; // Tahun baru, reset nomor urut
-      }
-    } else {
-      newNumber = 1; // Jika belum ada ID sebelumnya
-    }
-
-    const newNumberStr = newNumber.toString().padStart(4, "0");
-    return `${prefix}${currentYear}${newNumberStr}`;
-  }
-}
-
-const getInfoMahasiswaPAPerAngkatanByNIP = async (nip: string) => {
-  const result = await prisma.mahasiswa.groupBy({
-    by: ["nim"],
-    _count: {
-      nim: true,
-    },
-    orderBy: {
-      nim: "desc",
-    },
-    where: {
-      nip: nip,
-    },
-    take: 8,
-  });
-
-  // Langkah 2: Mengelompokkan hasil berdasarkan tahun dan menghitung jumlah
-  const groupedResult = result.reduce((acc, item) => {
-    const tahun = `20${item.nim.slice(1, 3)}`;
-
-    if (acc[tahun]) {
-      acc[tahun] += item._count.nim;
-    } else {
-      acc[tahun] = item._count.nim;
-    }
-
-    return acc;
-  }, {} as Record<string, number>);
-
-  // Langkah 3: Mengonversi objek menjadi array dan mengurutkannya
-  const formattedResult = Object.keys(groupedResult)
-    .map((tahun) => ({
-      tahun,
-      jumlah: groupedResult[tahun],
-    }))
-    .sort((a, b) => b.tahun.localeCompare(a.tahun)); // Mengurutkan tahun secara menurun
-
-  return formattedResult;
-};
-
-const getAllInfoSetoranByNip = async (nip: string) => {
-  try {
-    const result = await prisma.setoran.groupBy({
-      by: ["tgl_validasi"],
-      where: {
-        nip: nip,
-      },
-      _count: {
-        _all: true, // Menghitung total record untuk setiap grup
-      },
-      take: 8, // Membatasi hasil ke 8 record
-      orderBy: {
-        tgl_validasi: "asc", // Atau 'desc' tergantung urutan yang diinginkan
-      },
-    });
-
-    // Mengganti nama _count menjadi total
-    const formattedResult = result.map((r) => ({
-      x: r.tgl_validasi,
-      y: r._count._all,
+  public static async getInfoMahasiswaPAPerAngkatanByEmail (email: string) {
+    const result = await prisma.$queryRaw`
+      SELECT 
+          CONCAT('20', substring(nim FROM 2 FOR 2)) AS tahun, 
+          COUNT(substring(nim FROM 2 FOR 2)) AS total 
+      FROM 
+          mahasiswa 
+      WHERE 
+          nip=(SELECT nip FROM dosen WHERE email = ${email})
+      GROUP BY 
+          substring(nim FROM 2 FOR 2) 
+      ORDER BY 
+          substring(nim FROM 2 FOR 2) DESC
+      LIMIT 8;
+    `;
+  
+    return (result as { tahun: string; total: string }[]).map((item) => ({
+      ...item,
+      total: Number(item.total), // or item.total.toString() if you prefer strings
     }));
-    return formattedResult;
-  } catch (error) {
-    return error;
-  }
-};
+  };
 
-export {
-  IDGenerator,
-  getInfoMahasiswaPAPerAngkatanByNIP,
-  getAllInfoSetoranByNip,
-};
+  public static async getMahasiswaPAByEmail (email: string) {
+    return await prisma.$queryRaw`
+        SELECT 
+          nim, nama
+        FROM 
+          mahasiswa 
+        WHERE 
+          nip = (SELECT nip FROM dosen WHERE email = ${email})
+        ORDER BY nama;
+      `;
+  }
+
+  public static async checkExistingSetoran(nim: string, email_dosen_pa: string, nomorSurahInt: number) {
+    const result = await prisma.$queryRaw`
+      SELECT * FROM "setoran" 
+      WHERE "nim" = ${nim} 
+        AND "nip" = (SELECT "nip" FROM "dosen" WHERE "email" = ${email_dosen_pa}) 
+        AND "nomor_surah" = ${nomorSurahInt} 
+      LIMIT 1
+    `;
+    return !((result as any).length == 0);
+  }
+  
+  public static async postSetoran(nim: string, email_dosen_pa: string, nomor_surah: number, tgl_setoran: string) {
+    const idSetoran = DosenHelper.getIDSetoran();
+    const tglSetoran = tgl_setoran ? new Date(tgl_setoran) : new Date();
+    const tglValidasi = new Date();
+    
+    return await prisma.$queryRaw`
+      INSERT INTO 
+        "setoran" ("id", "tgl_setoran", "tgl_validasi", "nim", "nip", "nomor_surah") 
+      VALUES (
+        ${idSetoran}, 
+        ${tglSetoran}, 
+        ${tglValidasi}, 
+        ${nim}, 
+        (SELECT nip FROM dosen WHERE email=${email_dosen_pa}), 
+        ${nomor_surah}
+      );
+    `;    
+  };
+}
+export { DosenService };
