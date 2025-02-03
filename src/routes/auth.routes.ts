596c8c0e8e3b302f41798d0db1c69aa309e7ce49
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-03 12:02:33 +0700

    ⚙ refactor: removing jwt n protected auth from api

--- src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { postAkun } from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.post("/akun", postAkun);
-
-export default router;
