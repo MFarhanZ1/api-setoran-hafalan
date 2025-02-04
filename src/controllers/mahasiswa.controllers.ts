e7ed179402df703dc34aecfbd3a704e1f17e022b
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-04 15:45:35 +0700

    ⚙ chore: separating get mahasiswa route

--- src/controllers/mahasiswa.controllers.ts
+++ src/controllers/mahasiswa.controllers.ts
@@ -1,38 +1,90 @@
 import { Request, Response } from "express";
 import { MahasiswaService } from "../services/mahasiswa.services.js";
 
+interface CustomRequest extends Request {
+	email?: string;
+}
+
 class MahasiswaController {
-  public static async getInfoMahasiswaByEmail (req: Request, res: Response) {
-    const { email } = req.params;
-    try {
-      const resultInfoMahasiswaByEmail = await MahasiswaService.getInfoMahasiswaByEmail(email);
-      if (!resultInfoMahasiswaByEmail) {
-        return res.status(404).json({
-          response: false,
-          message: "Oops! data mahasiswa tidak ditemukan. 😭",
-        });
-      }
-      const resultRingkasanSetoranMahasiswaByNIM = await MahasiswaService.getRingkasanSetoranMahasiswaByNIM(resultInfoMahasiswaByEmail.nim);
-      const resultDetailSetoranMahasiswaByNIM = await MahasiswaService.getDetailSetoranMahasiswaByNIM(resultInfoMahasiswaByEmail.nim);    
-      res.status(200).json({
-        response: true,
-        message: "Berikut info detail mahasiswa beserta riwayat setoran-nya! 😁",
-        data: {
-          info: resultInfoMahasiswaByEmail,
-          setoran: {
-            ringkasan: resultRingkasanSetoranMahasiswaByNIM,
-            detail: resultDetailSetoranMahasiswaByNIM
-          }
-        }
-      });
-    } catch (error) {
-      console.error(`[ERROR] ${error}`);
-      return res.status(500).json({
-        response: false,
-        message: "Oops! ada kesalahan di server kami 😭",
-      });
-    }
-  };
+	public static async getInfoMahasiswaByEmail(req: Request, res: Response) {
+		const { email } = req.params;
+		try {
+			const resultInfoMahasiswaByEmail =
+				await MahasiswaService.getInfoMahasiswaByEmail(email);
+			if (!resultInfoMahasiswaByEmail) {
+				return res.status(404).json({
+					response: false,
+					message: "Oops! data mahasiswa tidak ditemukan. 😭",
+				});
+			}
+			const resultRingkasanSetoranMahasiswaByNIM =
+				await MahasiswaService.getRingkasanSetoranMahasiswaByNIM(
+					resultInfoMahasiswaByEmail.nim
+				);
+			const resultDetailSetoranMahasiswaByNIM =
+				await MahasiswaService.getDetailSetoranMahasiswaByNIM(
+					resultInfoMahasiswaByEmail.nim
+				);
+			res.status(200).json({
+				response: true,
+				message:
+					"Berikut info detail mahasiswa beserta riwayat setoran-nya! 😁",
+				data: {
+					info: resultInfoMahasiswaByEmail,
+					setoran: {
+						ringkasan: resultRingkasanSetoranMahasiswaByNIM,
+						detail: resultDetailSetoranMahasiswaByNIM,
+					},
+				},
+			});
+		} catch (error) {
+			console.error(`[ERROR] ${error}`);
+			return res.status(500).json({
+				response: false,
+				message: "Oops! ada kesalahan di server kami 😭",
+			});
+		}
+	}
+
+	public static async getSetoranSaya(req: Request, res: Response) {
+		const email = (req as CustomRequest).email!;
+		try {
+			const resultInfoMahasiswaByEmail =
+				await MahasiswaService.getInfoMahasiswaByEmail(email);
+			if (!resultInfoMahasiswaByEmail) {
+				return res.status(404).json({
+					response: false,
+					message: "Oops! data mahasiswa tidak ditemukan. 😭",
+				});
+			}
+			const resultRingkasanSetoranMahasiswaByNIM =
+				await MahasiswaService.getRingkasanSetoranMahasiswaByNIM(
+					resultInfoMahasiswaByEmail.nim
+				);
+			const resultDetailSetoranMahasiswaByNIM =
+				await MahasiswaService.getDetailSetoranMahasiswaByNIM(
+					resultInfoMahasiswaByEmail.nim
+				);
+			res.status(200).json({
+				response: true,
+				message:
+					"Berikut info detail mahasiswa beserta riwayat setoran-nya! 😁",
+				data: {
+					info: resultInfoMahasiswaByEmail,
+					setoran: {
+						ringkasan: resultRingkasanSetoranMahasiswaByNIM,
+						detail: resultDetailSetoranMahasiswaByNIM,
+					},
+				},
+			});
+		} catch (error) {
+			console.error(`[ERROR] ${error}`);
+			return res.status(500).json({
+				response: false,
+				message: "Oops! ada kesalahan di server kami 😭",
+			});
+		}
+	}
 }
 
 export { MahasiswaController };
