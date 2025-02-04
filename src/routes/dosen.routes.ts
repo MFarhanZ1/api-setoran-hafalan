1d6e6a858f2d4bccf26bc962ef3679e47e09933e
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-04 15:47:24 +0700

    ⚙ chore: editing routes

--- src/routes/dosen.routes.ts
+++ src/routes/dosen.routes.ts
@@ -1,18 +1,27 @@
 import express from "express";
 import { DosenController } from "../controllers/dosen.controllers.js";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get(
-  "/dosen/:email",
+  "/dosen/get-mahasiswa/:email",
+  AuthMiddleware.tokenValidation,
   DosenController.getInfoDosenByEmail
+);
+router.get(
+  "/dosen/pa-saya",
+  AuthMiddleware.tokenValidation,
+  DosenController.getPASaya
 );
 router.post(
   "/dosen/setor",
+  AuthMiddleware.tokenValidation,
   DosenController.postSetoran
 );
 router.delete(
   "/dosen/setor/:id_setoran",
+  AuthMiddleware.tokenValidation,
   DosenController.deleteSetoranByID
 );
 
--- src/routes/mahasiswa.routes.ts
+++ src/routes/mahasiswa.routes.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import { MahasiswaController } from "../controllers/mahasiswa.controllers.js";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get(
-  "/mahasiswa/:email",
+  "/mahasiswa/setoran/:email",
+  AuthMiddleware.tokenValidation,
   MahasiswaController.getInfoMahasiswaByEmail
 );
+
+router.get(
+  "/mahasiswa/setoran-saya",
+  AuthMiddleware.tokenValidation,
+  MahasiswaController.getSetoranSaya
+);
+
+
 
 export default router;
