fd57429ad57995a42cfd2643faee640fadb088f0
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-03-12 19:16:41 +0700

    ✨ refactor: update database schema and migrations for improved relationships and UUID usage

---
+++ prisma/migrations/20250312112620_init/migration.sql
@@ -0,0 +1,67 @@
+-- CreateEnum
+CREATE TYPE "type_label_surah" AS ENUM ('KP', 'SEMKP', 'DAFTAR_TA', 'SEMPRO', 'SIDANG_TA');
+
+-- CreateTable
+CREATE TABLE "dosen" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "nama" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "nip" VARCHAR(18) NOT NULL,
+
+    CONSTRAINT "dosen_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "mahasiswa" (
+    "nim" VARCHAR(11) NOT NULL,
+    "nama" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "id_dosen" UUID,
+
+    CONSTRAINT "mahasiswa_pkey" PRIMARY KEY ("nim")
+);
+
+-- CreateTable
+CREATE TABLE "setoran" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "tgl_setoran" DATE DEFAULT CURRENT_TIMESTAMP,
+    "tgl_validasi" DATE DEFAULT CURRENT_TIMESTAMP,
+    "nim" VARCHAR(11) NOT NULL,
+    "id_dosen" UUID NOT NULL,
+    "nomor_surah" INTEGER NOT NULL,
+
+    CONSTRAINT "setoran_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "surah" (
+    "nomor" INTEGER NOT NULL,
+    "nama" VARCHAR(25) NOT NULL,
+    "label" "type_label_surah",
+
+    CONSTRAINT "surah_pkey" PRIMARY KEY ("nomor")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "dosen_email_key" ON "dosen"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "dosen_nip_key" ON "dosen"("nip");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "mahasiswa_email_key" ON "mahasiswa"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "setoran_nim_nomor_surah_key" ON "setoran"("nim", "nomor_surah");
+
+-- AddForeignKey
+ALTER TABLE "mahasiswa" ADD CONSTRAINT "mahasiswa_id_dosen_fkey" FOREIGN KEY ("id_dosen") REFERENCES "dosen"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "setoran" ADD CONSTRAINT "setoran_id_dosen_fkey" FOREIGN KEY ("id_dosen") REFERENCES "dosen"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "setoran" ADD CONSTRAINT "fk_nim_setoran" FOREIGN KEY ("nim") REFERENCES "mahasiswa"("nim") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "setoran" ADD CONSTRAINT "fk_nomor_surah_setoran" FOREIGN KEY ("nomor_surah") REFERENCES "surah"("nomor") ON DELETE NO ACTION ON UPDATE NO ACTION;
