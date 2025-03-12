4a1fb14b1a8f236c5b37f0d7a565b5666d7eecb7
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-03-12 22:11:24 +0700

    ✨ refactor: simplify DosenHelper methods and improve validation logic for setoran

--- src/helpers/dosen.helpers.ts
+++ src/helpers/dosen.helpers.ts
@@ -1,24 +1,17 @@
 class DosenHelper {
-    public static getIDSetoran(): string {
-        const prefix = "SH";
-        const currentYear = new Date().getFullYear().toString().slice(-2); // Tahun 2 digit terakhir
+	public static validatePostSetoran(
+		nim: string,
+		nomor_surah: number
+	): boolean {
+		return (
+			!nim ||
+			!nomor_surah
+		);
+	}
 
-        // Menghasilkan 4 karakter acak alfanumerik
-        const randomChars = Math.random()
-            .toString(36)
-            .substring(2, 6)
-            .toUpperCase();
-
-        return `${prefix}${currentYear}${randomChars}`;
-    }
-
-    public static validatePostSetoran(nim: string, email_dosen_pa: string, nomor_surah: number): boolean {
-        return (!nim || !email_dosen_pa || !nomor_surah);
-    }
-
-    public static validateDeleteSetoran(id_setoran: string): boolean {
-        return (!id_setoran);
-    }
+	public static validateDeleteSetoran(id_setoran: string): boolean {
+		return !id_setoran;
+	}
 }
 
 export { DosenHelper };
